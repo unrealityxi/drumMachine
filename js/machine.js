14e158ef99c5cb9f3ff40c09c57d800d67b4d8dd
Dragan Roganović <unrealityxi@gmail.com>
2017-05-26 17:05:46 +0200

    added dynamic background

---
+++ js/machine.js
@@ -0,0 +1,37 @@
+(function(){
+  
+  "use strict";
+  const background = document.querySelector(".backgroundImg");
+  function playSound(e){
+    // Get the audio element corresponding to key
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    // Return if no such element
+    if (!audio){
+      return;
+    }
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    background.classList.add("animate");
+  }
+  
+  
+  // attach event listeners to end transitions
+  const keys = document.querySelectorAll(".key");
+  
+  function removeTransition(e){
+    // weed out all but transform
+    if (e.propertyName !== "transform"){
+      return;
+    }
+    this.classList.remove("playing");
+    background.classList.remove("animate");
+  }
+  
+  keys.forEach(key => {
+    key.addEventListener("transitionend", removeTransition);
+  });
+  window.addEventListener("keydown", playSound);
+})();
